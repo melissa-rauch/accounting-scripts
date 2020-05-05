ba843e26e5c4259497d02c4bf1b7bed1a6480ae5
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-04 21:43:26 -0700

    Completed print_melons function

--- melon_info.py
+++ melon_info.py
@@ -1,9 +1,17 @@
 """Print out all the melons in our inventory."""
 
-
+#import melon.py
 from melons import melons
 
 
 def print_melons(melons):
     """Print each melon with corresponding attribute information."""
-           
+    #For Melon (Keys) names in Dictionary, print the Melon name sin uppercase
+    for melon_names, attributes in melons.items():
+        print(melon_names.upper())
+        # For the various Values in attributes in Melon dictionary
+        #Print attribut:Value
+        for attribute, value in attributes.items():
+            print(f"{attribute}: {value}")
+#Call Function
+print_melons(melons) 
--- melons.py
+++ melons.py
@@ -6,28 +6,28 @@ melons = {
     "seedless" : True, 
     "flesh_color" : None, 
     "rind_color" : None, 
-    "average_lbs" : 6}
+    "average_lbs" : 6},
 
     'Crenshaw': 
     {"price": 2.00, 
     "seedless" : False, 
     "flesh_color" : None, 
     "rind_color" : None, 
-    "average_lbs" : 6}
+    "average_lbs" : 6},
 
     'Crane':
     {"price": 2.50, 
     "seedless" : False, 
     "flesh_color" : None, 
     "rind_color" : None, 
-    "average_lbs" : 6}
+    "average_lbs" : 6},
 
     'Casaba':
     {"price": 2.50, 
     "seedless" : False, 
     "flesh_color" : None, 
     "rind_color" : None, 
-    "average_lbs" : 5}
+    "average_lbs" : 5},
 
     'Cantaloupe':
     {"price": 0.99, 
